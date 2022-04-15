86aee9e83367e051a7cc30b24f7f1ad40882b188
tj-tieso <tj.tieso@gmail.com>
2022-04-15 12:33:24 -0500

    questions and choices generating randomly

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,85 +2,110 @@
 // var choiceEl = document.querySelector("#choice");
 var quizTimer = 0;
 var score = 0;
+var choiceEl = document.querySelector(".choices");
 var questionBoxEl = document.querySelector("#question-wrapper");
 var questionTitle = document.querySelector(".title");
 var startButtonEl = document.querySelector("#start-quiz");
+var questionOneChoices = ["stings", "boolean", "alerts", "numbers"];
+var questionTwoChoices = [
+  "quotes",
+  "curly brackets",
+  "parenthesis",
+  "square brackets",
+];
+var questionThreeChoices = [
+  "numbers and strings",
+  "other arrays",
+  "booleans",
+  "all of the above",
+];
+var questionFourChoices = [
+  "JavaScript",
+  "terminal/bash",
+  "for loops",
+  "console.log",
+];
+
 var availableQuestions = [];
+var currentQuestion = {};
 
 // question arrays
 var questionOptions = [
   {
     question: "Commonly used data types do not include:",
-    choice1: "stings",
-    choice2: "boolean",
-    choice3: "alerts",
-    choice4: "numbers",
-    answer: 3,
+    choice: Array.from(questionOneChoices),
   },
   {
-    question:
-      "The condition in an if / else statment is enclosed with ______ .",
-    choice1: "quotes",
-    choice2: "curly brackets",
-    choice3: "parenthesis",
-    choice4: "square brackets",
-    answer: 3,
+    question: "The condition in an if / else statment is enclosed with: ",
+    choice: Array.from(questionTwoChoices),
   },
   {
-    question: "Arrays in JavaScript can be used to store _____ .",
-    choice1: "numbers and strings",
-    choice2: "other arrays",
-    choice3: "booleans",
-    choice4: "all of the above",
-    answer: 3,
+    question: "Arrays in JavaScript can be used to store: ",
+    choice: Array.from(questionThreeChoices),
   },
   {
     question:
       "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choice1: "JavaScript",
-    choice2: "terminal/bash",
-    choice3: "for loops",
-    choice4: "console.log",
-    answer: 4,
+    choice: Array.from(questionFourChoices),
   },
 ];
 
 //function to start game
 var quizStart = function () {
+  score = 0;
+  availableQuestions = [...questionOptions];
+  // console.log(availableQuestions); // missing one question
+
+  // start timer
+
   //removes paragraph
   var paragraph = document.getElementById("para");
   paragraph.remove();
 
-  // Delete Start Button
+  // removes Start Button
   var buttonRemove = document.getElementById("start-quiz");
   buttonRemove.remove();
-  // Shift <h1> to side
-  // start timer
+
+  //get first/new
   generateQuestions();
 };
 
 var generateQuestions = function () {
-  // question from aray
-  questionTitle.textContent = "This will be a question.";
+  //in no questions left, end the game
+  if (availableQuestions === 0) {
+    endGame();
+  }
 
-  //generate 4 question boxes
-  // assign each a unique id
-  // get info from array
+  //get random question from array
+  var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  questionTitle.textContent = currentQuestion.question;
 
-  var choiceEl = document.createElement("li");
-  choiceEl.setAttribute("id", "choice-");
-  choiceEl.setAttribute("class", "choices");
-  choiceEl.textContent = "this is a test"; // text content from Question array
+  // generate question boxes / get info from array / assign each a unique id
+  for (var i = 0; i < currentQuestion.choice.length; i++) {
+    // move to startGame() ??
+    var choiceEl = document.createElement("li");
+    choiceEl.setAttribute("id", "choice-" + i);
+    choiceEl.setAttribute("class", "choices");
+    choiceEl.textContent = currentQuestion.choice[i];
 
-  // append choiceEl to ul #questions
-  questionBoxEl.appendChild(choiceEl);
-  // load new question
-  // // getNewQuestion();
+    // append choiceEl to ul
+    questionBoxEl.appendChild(choiceEl);
+  }
+  // splice out currentQuestion, so it won't be used again
+  availableQuestions.splice(questionIndex, 1);
+  console.log(availableQuestions);
+
+  // load new question after answer
+
+  //
 };
 
-// var getNewQuestion = function ();
-
 var resetGame = function () {};
+
+var endGame = function () {
+  console.log("we out folks");
+};
 
 // on click, quizStart()
 startButtonEl.addEventListener("click", quizStart);
