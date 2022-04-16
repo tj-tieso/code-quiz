622fae25cac4f30a00909c04b9297b7eeff4f0b0
tj-tieso <tj.tieso@gmail.com>
2022-04-16 13:34:45 -0500

    new questions loading, display right/wrong

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 //global variables
 // var choiceEl = document.querySelector("#choice");
+var questionIndex = 0;
 var quizTimer = 0;
 var score = 0;
 var choiceEl = document.querySelector(".choices");
+var quizBoxEl = document.querySelector(".quiz-wrapper");
 var questionBoxEl = document.querySelector("#question-wrapper");
 var questionTitle = document.querySelector(".title");
 var startButtonEl = document.querySelector("#start-quiz");
-var questionOneChoices = ["stings", "boolean", "alerts", "numbers"];
+var questionOneChoices = ["strings", "boolean", "alerts", "numbers"];
 var questionTwoChoices = [
   "quotes",
   "curly brackets",
@@ -27,33 +29,36 @@ var questionFourChoices = [
 ];
 
 var availableQuestions = [];
-var currentQuestion = {};
+var currentQuestion;
 
 // question arrays
 var questionOptions = [
   {
     question: "Commonly used data types do not include:",
     choice: Array.from(questionOneChoices),
+    answer: "alerts",
   },
   {
     question: "The condition in an if / else statment is enclosed with: ",
     choice: Array.from(questionTwoChoices),
+    answer: "parenthesis",
   },
   {
     question: "Arrays in JavaScript can be used to store: ",
     choice: Array.from(questionThreeChoices),
+    answer: "all of the above",
   },
   {
     question:
       "A very useful tool used during development and debugging for printing content to the debugger is:",
     choice: Array.from(questionFourChoices),
+    answer: "console.log",
   },
 ];
-
+console.log(questionOptions);
 //function to start game
 var quizStart = function () {
   score = 0;
-  availableQuestions = [...questionOptions];
   // console.log(availableQuestions); // missing one question
 
   // start timer
@@ -71,39 +76,57 @@ var quizStart = function () {
 };
 
 var generateQuestions = function () {
-  //in no questions left, end the game
-  if (availableQuestions === 0) {
-    endGame();
-  }
-
   //get random question from array
-  var questionIndex = Math.floor(Math.random() * availableQuestions.length);
-  currentQuestion = availableQuestions[questionIndex];
+  currentQuestion = questionOptions[questionIndex];
   questionTitle.textContent = currentQuestion.question;
-
+  questionBoxEl.innerHTML = "";
   // generate question boxes / get info from array / assign each a unique id
   for (var i = 0; i < currentQuestion.choice.length; i++) {
     // move to startGame() ??
     var choiceEl = document.createElement("li");
     choiceEl.setAttribute("id", "choice-" + i);
     choiceEl.setAttribute("class", "choices");
+    choiceEl.setAttribute("data-choice", currentQuestion.choice[i]);
     choiceEl.textContent = currentQuestion.choice[i];
-
+    choiceEl.onclick = function (event) {
+      var choice = this.getAttribute("data-choice");
+      console.log(choice);
+      if (choice !== currentQuestion.answer) {
+        // displayResult(choice, currentQuestion.answer, "wrong");
+        displayResult(choice, currentQuestion.answer, "wrong");
+      } else {
+        // console.log(choice + " is correct");
+        displayResult(choice, currentQuestion.answer, "right");
+      }
+      questionIndex++;
+      if (questionIndex === questionOptions.length) {
+        endGame();
+      } else {
+        generateQuestions();
+      }
+      // load new question after answer
+      // return generateQuestions();
+    };
     // append choiceEl to ul
     questionBoxEl.appendChild(choiceEl);
   }
-  // splice out currentQuestion, so it won't be used again
-  availableQuestions.splice(questionIndex, 1);
-  console.log(availableQuestions);
+};
 
-  // load new question after answer
+var displayResult = function (choice, answer, result) {
+  var resultEl = document.createElement("h3");
+  resultEl.textContent = `${choice} is ${result}`;
+  quizBoxEl.appendChild(resultEl);
 
-  //
+  setTimeout(function () {
+    resultEl.textContent = "";
+  }, 1000);
 };
 
 var resetGame = function () {};
 
 var endGame = function () {
+  questionBoxEl.innerHTML = "";
+  questionTitle.textContent = "";
   console.log("we out folks");
 };
 
